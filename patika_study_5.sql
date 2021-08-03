eb360091165411ac32ef1dcb8f26bf2b339cb175
busesenvardar <61200732+busesenvardar@users.noreply.github.com>
2021-08-03 17:17:53 +0300

    Add files via upload

---
+++ patika_study_5.sql
@@ -0,0 +1,18 @@
+--1
+select title from film
+where title like '%n' 
+order by length desc
+limit 5;
+
+--2
+select title from film
+where title like '%n' 
+order by length asc
+limit 5
+offset 5;
+
+--3
+select * from customer
+where store_id=1
+order by last_name desc 
+limit 4;
